22362fc5573b9818d4c309f5b76cf89de6d96534
Misha-Solt <mikhail.us@gmail.com>
2024-04-23 17:00:10 +0200

    Server side ended

--- lift-off-part-3/odyssey-lift-off-part3/server/src/schema.js
+++ lift-off-part-3/odyssey-lift-off-part3/server/src/schema.js
@@ -1,34 +1,51 @@
-const gql = require('graphql-tag');
+const gql = require('graphql-tag')
 
 const typeDefs = gql`
-  type Query {
-    "Query to get tracks array for the homepage grid"
-    tracksForHome: [Track!]!
-  }
+    type Query {
+        "Query to get tracks array for the homepage grid"
+        tracksForHome: [Track!]!
+        "Fetch a specific track, provided a track's ID"
+        track(id: ID!): Track!
+    }
 
-  "A track is a group of Modules that teaches about a specific topic"
-  type Track {
-    id: ID!
-    "The track's title"
-    title: String!
-    "The track's main Author"
-    author: Author!
-    "The track's illustration to display in track card or track page detail"
-    thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
-    "The number of modules this track contains"
-    modulesCount: Int
-  }
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+        "The track's title"
+        title: String!
+        "The track's main Author"
+        author: Author!
+        "The track's illustration to display in track card or track page detail"
+        thumbnail: String
+        "The track's approximate length to complete, in minutes"
+        length: Int
+        "The number of modules this track contains"
+        modulesCount: Int
+        "The track's compleate description"
+        description: String
+        "The number of times a track has been viewed"
+        numberOfViews: Int
+        "Modules"
+        modules: [Module!]!
+    }
 
-  "Author of a complete Track or a Module"
-  type Author {
-    id: ID!
-    "Author's first and last name"
-    name: String!
-    "Author's profile picture"
-    photo: String
-  }
-`;
+    "A module is a single unit of teaching."
+    type Module {
+        id: ID!
+        "The module title"
+        title: String!
+        "The module length"
+        length: Int
+    }
 
-module.exports = typeDefs;
+    "Author of a complete Track or a Module"
+    type Author {
+        id: ID!
+        "Author's first and last name"
+        name: String!
+        "Author's profile picture"
+        photo: String
+    }
+`
+
+module.exports = typeDefs
